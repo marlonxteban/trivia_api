937067e29ab5e0ff2ccece2280490dce1366df8d
marlonxteban <marlonxteban@gmail.com>
2021-01-10 20:59:21 -0500

    Add database files to set up local environment

---
+++ database/00insertCategories.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.categories(
+	id, type)
+	VALUES (1, 'Science');
+
+INSERT INTO public.categories(
+	id, type)
+	VALUES (2, 'Art');
+	
+INSERT INTO public.categories(
+	id, type)
+	VALUES (3, 'Geography');
+	
+INSERT INTO public.categories(
+	id, type)
+	VALUES (4, 'History');
+
+INSERT INTO public.categories(
+	id, type)
+	VALUES (5, 'Entertainment');
+
+INSERT INTO public.categories(
+	id, type)
+	VALUES (6, 'Sports');
---
+++ database/01InsertQuestions.sql
@@ -0,0 +1,246 @@
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		5, 
+		E'Whose autobiography is entitled \'I Know Why the Caged Bird Sings\'?', 
+		'Maya Angelou', 
+		2, 
+		4);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		9, 
+		E'What boxer\'s original name is Cassius Clay?', 
+		'Muhammad Ali', 
+		6, 
+		1);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		2, 
+		E'What movie earned Tom Hanks his third straight Oscar nomination, in 1996?', 
+		'Apollo 13', 
+		5, 
+		4);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		4, 
+		E'What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?', 
+		'Apollo 13', 
+		5, 
+		4);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		6, 
+		E'What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?', 
+		'Edward Scissorhands', 
+		5, 
+		3);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		10, 
+		E'Which is the only team to play in every soccer World Cup tournament?', 
+		'Brazil', 
+		6, 
+		3);
+		
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		11, 
+		E'Which country won the first ever soccer World Cup in 1930?', 
+		'Uruguay', 
+		6, 
+		4);
+		
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		12, 
+		E'Who invented Peanut Butter?', 
+		'George Washington Carver', 
+		4, 
+		2);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		13, 
+		E'What is the largest lake in Africa?', 
+		'Lake Victoria', 
+		3, 
+		2);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		14, 
+		E'In which royal palace would you find the Hall of Mirrors?', 
+		'The Palace of Versailles', 
+		3, 
+		3);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		15, 
+		E'The Taj Mahal is located in which Indian city?', 
+		'Agra', 
+		3, 
+		2);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		16, 
+		E'Which Dutch graphic artist–initials M C was a creator of optical illusions?', 
+		'Escher', 
+		2, 
+		1);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		17, 
+		E'La Giaconda is better known as what?', 
+		'Mona Lisa', 
+		2, 
+		3);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		18, 
+		E'How many paintings did Van Gogh sell in his lifetime?', 
+		'One', 
+		2, 
+		4);
+		
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		19, 
+		E'Which American artist was a pioneer of Abstract Expressionism, and a leading exponent of action painting?', 
+		'Jackson Pollock', 
+		2, 
+		2);
+		
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		20, 
+		E'What is the heaviest organ in the human body?', 
+		'The Liver', 
+		1, 
+		4);
+		
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		21, 
+		E'Who discovered penicillin?', 
+		'Alexander Fleming', 
+		3, 
+		1);
+		
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		22, 
+		E'Hematology is a branch of medicine involving the study of what?', 
+		'Blood', 
+		1, 
+		4);
+
+INSERT INTO public.questions(
+	id, 
+	question, 
+	answer, 
+	category_id, 
+	difficulty)
+	VALUES (
+		23, 
+		E'Which dung beetle was worshipped by the ancient Egyptians?', 
+		'Scarab', 
+		4, 
+		4);
